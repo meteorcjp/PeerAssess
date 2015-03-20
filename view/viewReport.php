4863cd3417f9dc4de323c8388197ea54b9b7e75a
Ernest Chen <jpchen@Ernests-MacBook-Air.local>
2015-03-20 12:45:21 +0000

    add box

--- view/viewReport.php
+++ view/viewReport.php
@@ -30,6 +30,30 @@
 	<?php
 	require_once './stuHeader.php';
 	?>
+
+	<div id="sucLg" class="modal fade bs-example-modal-sm" tabindex="-1"
+	role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-lg">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+				<h4 id="rT" class="modal-title">Hint Info</h4>
+			</div>
+
+			<div class="modal-body">
+				<p id="causeLg">Operation Successfully!</p>
+			</div>
+
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">OK</button>
+			</div>
+
+		</div>
+	</div>
+</div>
+
 	<div class="container">
 
 		<div class="starter-template">
