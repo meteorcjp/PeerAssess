dcb4d6bb100a2a5c2fa11e79c53d3be4ca113cd4
Ernest Chen <jpchen@Ernests-MacBook-Air.local>
2015-03-20 13:10:41 +0000

    update

--- view/viewReport.php
+++ view/viewReport.php
@@ -17,11 +17,7 @@
 	$(function(){
 		$("#viLi").addClass("active");
 	});
-	function showContent(title,content){
-		$("#rT").html(title);
-		$("#causeLg").html(content);
-		$("#sucLg").modal("toggle");
-	}
+
 	</script>
 </head>
 
@@ -31,28 +27,6 @@
 	require_once './stuHeader.php';
 	?>
 
-	<div id="sucLg" class="modal fade bs-example-modal-sm" tabindex="-1"
-	role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-	<div class="modal-dialog modal-lg">
-		<div class="modal-content">
-			<div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-					<span aria-hidden="true">&times;</span>
-				</button>
-				<h4 id="rT" class="modal-title">Hint Info</h4>
-			</div>
-
-			<div class="modal-body">
-				<p id="causeLg">Operation Successfully!</p>
-			</div>
-
-			<div class="modal-footer">
-				<button type="button" class="btn btn-default" data-dismiss="modal">OK</button>
-			</div>
-
-		</div>
-	</div>
-</div>
 
 	<div class="container">
 
@@ -82,7 +56,6 @@
 								$content = nl2br($content);
 								$title = $row[1];
 							?>
-							<a style="margin-left:50px;" href="javascript:showContent('','')">view this report</a>
 						</div>
 					</div>
 
