37846cbd78c8e418d292aa1b20bc1f65381038fb
Ernest Chen <jpchen@Ernests-MacBook-Air.local>
2015-03-20 01:24:10 +0000

    guid

--- controller/uploadController.php
+++ controller/uploadController.php
@@ -1,4 +1,23 @@
 <?php
+
+function guid(){
+  if (function_exists('com_create_guid')){
+    return com_create_guid();
+  }else{
+    mt_srand((double)microtime()*10000);//optional for php 4.2.0 and up.
+    $charid = strtoupper(md5(uniqid(rand(), true)));
+    $hyphen = chr(45);// "-"
+    $uuid = chr(123)// "{"
+    .substr($charid, 0, 8).$hyphen
+    .substr($charid, 8, 4).$hyphen
+    .substr($charid,12, 4).$hyphen
+    .substr($charid,16, 4).$hyphen
+    .substr($charid,20,12)
+    .chr(125);// "}"
+    return $uuid;
+  }
+}
+
 session_start();
 include_once '../model/Report.php';
 $title = $_POST['title'];
@@ -8,7 +27,14 @@ $time = date('Y-m-d',time());
 $name = $_FILES["file"]["name"];
 
 $imageFileType = pathinfo($name,PATHINFO_EXTENSION);
-// if($imageFileType == "xml"){
+
+$index = strrpos($name,".");
+$name1 = substr($name, 0, $index);
+$name1.=guid();
+$name2 = substr($name, $index);
+$name = $name1.$name2;
+
+if($imageFileType == "xml"){
   //save the file
   $result = move_uploaded_file($_FILES["file"]["tmp_name"],"../files/".$name);
 
@@ -22,8 +48,8 @@ $imageFileType = pathinfo($name,PATHINFO_EXTENSION);
   else{
     header("Location:../view/homepage.php?info=Fail to upload");
   }
-// }
-// else {
-//   header("Location:../view/homepage.php?info=Wrong file type: ".$imageFileType);
-// }
+}
+else {
+  header("Location:../view/homepage.php?info=Wrong file type: ".$imageFileType);
+}
 ?>
