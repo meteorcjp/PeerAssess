7e617788d8ecc0a0eaa71cc428e59e6c1f4e1f10
Ernest Chen <jpchen@Ernests-MacBook-Air.local>
2015-03-20 01:29:02 +0000

    txtxml

--- controller/uploadController.php
+++ controller/uploadController.php
@@ -34,22 +34,23 @@ $name1.=guid();
 $name2 = substr($name, $index);
 $name = $name1.$name2;
 
-if($imageFileType == "xml"){
-  //save the file
+// if($imageFileType == "xml"){
+//   //save the file
   $result = move_uploaded_file($_FILES["file"]["tmp_name"],"../files/".$name);
 
   if($result == true){
-    $xml=simplexml_load_file("../files/".$name) or die("Error: Cannot create object");
-    $content = $xml->content;
+    // $xml=simplexml_load_file("../files/".$name) or die("Error: Cannot create object");
+    // $content = $xml->content;
     //insert into database
+    $content = 'c';
     addReport($title, $name, $uploader, $desc, $content, $time);
     header("Location:../view/homepage.php?show=1");
   }
   else{
     header("Location:../view/homepage.php?info=Fail to upload");
   }
-}
-else {
-  header("Location:../view/homepage.php?info=Wrong file type: ".$imageFileType);
-}
+//}
+// else {
+//   header("Location:../view/homepage.php?info=Wrong file type: ".$imageFileType);
+// }
 ?>
