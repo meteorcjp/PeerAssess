1c217e8c8148b579980f2ef148b507061aca19cb
Ernest Chen <jpchen@Ernests-MacBook-Air.local>
2015-03-20 12:47:15 +0000

    no rename

--- controller/uploadController.php
+++ controller/uploadController.php
@@ -1,22 +1,22 @@
 <?php
 
-function guid(){
-  if (function_exists('com_create_guid')){
-    return com_create_guid();
-  }else{
-    mt_srand((double)microtime()*10000);//optional for php 4.2.0 and up.
-    $charid = strtoupper(md5(uniqid(rand(), true)));
-    $hyphen = chr(45);// "-"
-    $uuid = chr(123)// "{"
-    .substr($charid, 0, 8).$hyphen
-    .substr($charid, 8, 4).$hyphen
-    .substr($charid,12, 4).$hyphen
-    .substr($charid,16, 4).$hyphen
-    .substr($charid,20,12)
-    .chr(125);// "}"
-    return $uuid;
-  }
-}
+// function guid(){
+//   if (function_exists('com_create_guid')){
+//     return com_create_guid();
+//   }else{
+//     mt_srand((double)microtime()*10000);//optional for php 4.2.0 and up.
+//     $charid = strtoupper(md5(uniqid(rand(), true)));
+//     $hyphen = chr(45);// "-"
+//     $uuid = chr(123)// "{"
+//     .substr($charid, 0, 8).$hyphen
+//     .substr($charid, 8, 4).$hyphen
+//     .substr($charid,12, 4).$hyphen
+//     .substr($charid,16, 4).$hyphen
+//     .substr($charid,20,12)
+//     .chr(125);// "}"
+//     return $uuid;
+//   }
+// }
 
 session_start();
 include_once '../model/Report.php';
@@ -31,18 +31,18 @@ $content = 'n';
 //   return;
 // }
 
-$index = strrpos($name,".");
-$name1 = substr($name, 0, $index);
-$name1.=guid();
-$name2 = substr($name, $index);
-$name_str = $name1.$name2;
+// $index = strrpos($name,".");
+// $name1 = substr($name, 0, $index);
+// $name1.=guid();
+// $name2 = substr($name, $index);
+// $name_str = $name1.$name2;
 
 
 //save the file
-move_uploaded_file($_FILES["file"]["tmp_name"],"../files/".$name_str);
+move_uploaded_file($_FILES["file"]["tmp_name"],"../files/".$name);
 // $xml = simplexml_load_file("../files/".$name_string) or die("Error: Cannot create object");
 // $content = $xml->content;
 //insert into database
-addReport($title, $name_str, $uploader, $desc, $content, $time);
+addReport($title, $name, $uploader, $desc, $content, $time);
 header("Location:../view/homepage.php?show=1");
 ?>
