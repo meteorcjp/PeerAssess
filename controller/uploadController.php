b7374c3cd2c1fc64678ef30bd3aa83ef9028f2af
Ernest Chen <jpchen@Ernests-MacBook-Air.local>
2015-03-20 01:15:43 +0000

    upload2

--- controller/uploadController.php
+++ controller/uploadController.php
@@ -9,17 +9,21 @@ $name = $_FILES["file"]["name"];
 
 $imageFileType = pathinfo($name,PATHINFO_EXTENSION);
 if($imageFileType == "xml"){
-//save the file
-$result = move_uploaded_file($_FILES["file"]["tmp_name"],"../files/".$name);
+  //save the file
+  $result = move_uploaded_file($_FILES["file"]["tmp_name"],"../files/".$name);
 
-if($result == true){
-  $xml=simplexml_load_file("../files/".$name) or die("Error: Cannot create object");
-  $content = $xml->content;
-  //insert into database
-  addReport($title, $name, $uploader, $desc, $content, $time);
-  header("Location:../view/homepage.php?show=1");
+  if($result == true){
+    $xml=simplexml_load_file("../files/".$name) or die("Error: Cannot create object");
+    $content = $xml->content;
+    //insert into database
+    addReport($title, $name, $uploader, $desc, $content, $time);
+    header("Location:../view/homepage.php?show=1");
+  }
+  else{
+    header("Location:../view/homepage.php?info=Fail to upload");
+  }
 }
-else{
-  header("Location:../view/homepage.php?info=Fail to upload");
+else {
+  header("Location:../view/homepage.php?info=Wrong file type: ".$imageFileType);
 }
 ?>
